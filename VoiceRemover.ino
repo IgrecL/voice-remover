98d63acdeb721c6ff3b8f920e8cdccaec66e496a
IgrecL <ngowg5p3v@relay.firefox.com>
2023-02-24 11:47:22 +0100

    Button to switch between tracks in the SD card

--- VoiceRemover.ino
+++ VoiceRemover.ino
@@ -26,13 +26,23 @@ AudioControlSGTL5000     sgtl5000_1;
 #define BUTTON_PIN2       3
 #define BUTTON_PIN3       4
 
+// Buttons handling
 int last = 0;
-int mode = 0;
+int mode = 1;
+int songIndex = 0;
+int songStop = 0;
 int button1Pressed = 0;
 int button2Pressed = 0;
 int button3Pressed = 0;
 
+// List of songs in the SD card
+#define MAX_SONGS         10
+String files[MAX_SONGS];
+int count = 0;
+
 void setup() {
+
+  // Setup parameters
   Serial.begin(9600);
   AudioMemory(12);
   sgtl5000_1.enable();
@@ -40,6 +50,8 @@ void setup() {
   pinMode(BUTTON_PIN1, INPUT);
   pinMode(BUTTON_PIN2, INPUT);
   pinMode(BUTTON_PIN3, INPUT);
+
+  // SD initialization
   SPI.setMOSI(SDCARD_MOSI_PIN);
   SPI.setSCK(SDCARD_SCK_PIN);
   if (!(SD.begin(SDCARD_CS_PIN))) {
@@ -48,13 +60,40 @@ void setup() {
       delay(500);
     }
   }
+
+  // Listing files and putting them in the array
+  count = 0;
+  File dir = SD.open("/Songs/");
+  while(true) {
+    File entry = dir.openNextFile();
+    if (!entry) {
+      break;
+    }
+    String name = entry.name();
+    Serial.println(name);
+    files[count] = name;
+    count++;     
+    entry.close();
+  }
+}
+
+void loop() {
+  if (mode == 1) {
+    char* filename = "";
+    strcat(filename, "Songs/");
+    strcat(filename, files[songIndex].c_str());
+    Serial.println(filename);
+    playFile(filename);
+  }
+  delay(100);
+  update();
 }
 
 void update() {
   
   // Update parameters values
   dsp.setParamValue("stereo", analogRead(A0));
-  dsp.setParamValue("volume", analogRead(A1));
+  sgtl5000_1.volume(analogRead(A1)/1500.0);
   Serial.println(analogRead(A0));
   Serial.println(analogRead(A1));
 
@@ -83,6 +122,14 @@ void update() {
     button2Pressed = 0;
   }
   
+  // Button to switch to next song in the SD card
+  Serial.println(digitalRead(BUTTON_PIN3));
+  if ((digitalRead(BUTTON_PIN3) == 1) && (button3Pressed == 0)) {
+    songStop = 1;
+    button3Pressed = 1;
+  } else if (digitalRead(BUTTON_PIN3) == 0) {
+    button3Pressed = 0;
+  }
   
   Serial.println(digitalRead(BUTTON_PIN3));
   Serial.println();
@@ -99,18 +146,12 @@ void playFile(const char *filename)
   delay(25);
   while (playWav.isPlaying()) {
     update();
-    if (mode == 0) {
+    if ((mode == 0) || (songStop)) {
       playWav.stop();
+      songStop = 0;
     }
   }
+  songIndex = (songIndex + 1) % count;
   patchCord1.connect();
   patchCord2.connect();
-}
-
-void loop() {
-  if (mode == 1) {
-    playFile("EXAMPLE.WAV");
-  }
-  delay(100);
-  update();
 }
